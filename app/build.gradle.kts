33e27e450f76baf691f270e72739a1a1492ad3e9
C-H-Rm <veeningenthijs@gmail.com>
2025-03-08 16:27:13 +0100

    added newpipe extractor dependancy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,14 @@ android {
     }
 }
 
+
+
+
+
 dependencies {
 
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0-RC")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.squareup.okhttp3:okhttp:3.8.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +73,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.github.teamnewpipe:NewPipeExtractor:0.24.4")
 }
--- app/src/main/java/com/chrm/miscmusic/MainActivity.kt
+++ app/src/main/java/com/chrm/miscmusic/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.chrm.miscmusic.ui.theme.MiscmusicTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,6 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -30,12 +44,51 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+//fun SongFeedLoadResult() {
+//    for (layout in layouts) {
+//        val en_title: String = layout.title?.getString("en-GB") ?: "?"
+//        val ja_title: String = layout.title?.getString("ja-JP") ?: "?"
+//
+//        println("Layout $en_title / $ja_title has ${layout.items.size} items")
+//    }
+//} SongFeedLoadResult()
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "klik")
+        }
+    }
+    Row (
+        modifier = Modifier
+            .background(colorScheme.background)
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        for (i in 1..5) {
+            Column(
+                modifier = Modifier
+                    //.size(width = 300.dp)
+                    .padding(5.dp)
+                    .background(colorScheme.primary)
+            ) {
+                for (i in 1..5) {
+                    Text(
+                        text = "$i. Hello $name!",
+                    )
+                }
+            }
+        }
+    }
+
+
+
 }
 
 @Preview(showBackground = true)
